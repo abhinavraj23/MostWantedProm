7f842452b54c5675700a97c1d83660acbe649f8c
abhinavraj23 <abhinav.23.april@gmail.com>
2018-02-18 03:05:56 +0530

    Created rules

---
+++ app/src/main/java/com/abhinav/mostwantedprom/Rule3Activity.java
@@ -0,0 +1,13 @@
+package com.abhinav.mostwantedprom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Rule3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rule3);
+    }
+}

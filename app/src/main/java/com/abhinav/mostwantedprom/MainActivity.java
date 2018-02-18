4129575203d6d32e5860c09b1904ff306492764d
abhinavraj23 <abhinav.23.april@gmail.com>
2018-02-18 20:56:38 +0530

    Created auto detect feature fto check if read RULES

--- app/src/main/java/com/abhinav/mostwantedprom/MainActivity.java
+++ app/src/main/java/com/abhinav/mostwantedprom/MainActivity.java
@@ -1,6 +1,7 @@
 package com.abhinav.mostwantedprom;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,18 +9,27 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
      Button proceed;
-    
+     boolean hasRead = false;
+    public static String globalPreferenceName="ACTIVITY";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        proceed = (Button) findViewById(R.id.button);
-        proceed.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,Rule1Activity.class));
-                finish();
-            }
-        });
+        SharedPreferences skip = getSharedPreferences(globalPreferenceName,MODE_PRIVATE);
+        hasRead = skip.getBoolean("ACTIVITY",hasRead);
+        if(!hasRead) {
+            proceed = (Button) findViewById(R.id.button);
+            proceed.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this, Rule1Activity.class));
+                    finish();
+                }
+            });
+        }
+        else{
+            startActivity(new Intent(MainActivity.this, GenderActivity.class));
+            finish();
+        }
     }
 }
--- app/src/main/java/com/abhinav/mostwantedprom/Rule4Activity.java
+++ app/src/main/java/com/abhinav/mostwantedprom/Rule4Activity.java
@@ -1,6 +1,7 @@
 package com.abhinav.mostwantedprom;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,18 +9,21 @@ import android.widget.Button;
 
 public class Rule4Activity extends AppCompatActivity {
     Button proceed, back;
-    static Rule4Activity INSTANCE;
+    public static String globalPreferenceName="ACTIVITY";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rule4);
-        INSTANCE=this;
         proceed =(Button) findViewById(R.id.proceed);
         back = (Button) findViewById(R.id.back);
         proceed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Rule4Activity.this,GenderActivity.class));
+                SharedPreferences skip = getSharedPreferences(globalPreferenceName,MODE_PRIVATE);
+                SharedPreferences.Editor editor = skip.edit();
+                editor.putBoolean("ACTIVITY",true);
+                editor.commit();
                 finish();
             }
         });
@@ -31,8 +35,5 @@ public class Rule4Activity extends AppCompatActivity {
             }
         });
     }
-    public Rule4Activity getActivityInstance(){return INSTANCE;}
-    public boolean hasReaded(){
-        return true;
-    }
+
 }
